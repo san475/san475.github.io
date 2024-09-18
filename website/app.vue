17adbde29a6c6f0e4c8c449a7d30d5c2aa364ab9
Francisco Esteve <franj696@gmail.com>
2024-09-18 13:16:19 -0500

    some editing and more setup, before build

--- website/app.vue
+++ website/app.vue
@@ -1,6 +1,38 @@
+<script setup>
+
+import {reactive, onMounted} from 'vue'
+
+const counter = reactive({count: 0});
+
+onMounted(() => setInterval(() => {
+  console.log('test', counter)
+  counter.count++;
+}, 1000))
+
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="center">
+      <h1>Francisco Website test</h1>
+      <h2> time here: {{ counter.count }}</h2>
+    </div>
   </div>
 </template>
+
+<style>
+body {
+  background-color: black;
+
+}
+
+.center {
+  padding: 32px;
+  margin: 32px;
+  background-color: white;
+
+}
+
+h1 {
+  color: gray;
+}
+</style>
