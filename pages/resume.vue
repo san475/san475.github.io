7f6ccfbb064309ae3089d42bb7854a4f842568b7
Francisco Esteve <franj696@gmail.com>
2024-10-04 17:42:52 -0500

    Removed default layout from resume page, default layout needs refactoring

--- pages/resume.vue
+++ pages/resume.vue
@@ -1,10 +1,17 @@
-
 <script>
+definePageMeta({
+  layout: false,
+});
 </script>
 
 
 <template>
-  <iframe src="https://docs.google.com/document/d/e/2PACX-1vRSGD0TaAmqbK_nLLDEmC5A94w-Bwpjp1O6cbozhPSpMtZf8GJ2vhry95XdLu1dWA/pub?embedded=true"></iframe>
+  <NuxtLayout name="game">
+    <body>
+      <iframe
+        src="https://docs.google.com/document/d/e/2PACX-1vRSGD0TaAmqbK_nLLDEmC5A94w-Bwpjp1O6cbozhPSpMtZf8GJ2vhry95XdLu1dWA/pub?embedded=true"></iframe>
+    </body>
+  </NuxtLayout>
 </template>
 
 <style>
